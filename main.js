e35d482fd68584c61bcc9b1cdefb828bd74a7b10
Ammar Husain <ammo700@gmail.com>
2017-01-16 17:55:26 -0500

    Able to create road boundary graphics, two line points centered in screen

--- main.js
+++ main.js
@@ -5,8 +5,34 @@ var Container = PIXI.Container,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
     Sprite = PIXI.Sprite;
+    Graphics = PIXI.Graphics;
 
-var renderer = autoDetectRenderer(720, 720, {antialias: true, resolution: 1});
+var renderer = autoDetectRenderer(720, 720, {resolution: 1});
 var stage = new Container();
-renderer.render(stage);
 document.body.appendChild(renderer.view);
+
+//Road boundaries
+var lpoints = [
+	[ 310, 75, 310, 720-75],
+	[ 410, 75, 410, 720-75],
+];
+
+var lines = [];
+
+function setup() {
+	var i;
+	for(i = 0; i < lpoints.length; i++){
+		var ln = new Graphics();
+		ln.lineStyle(20, 0xFFFFFF, 1);
+		ln.moveTo(lpoints[i][0], lpoints[i][1]);
+		ln.lineTo(lpoints[i][2], lpoints[i][3]);
+		lines.push(ln);
+	}
+	for(i = 0; i < lines.length; i++){
+		stage.addChild(lines[i]);
+	}	
+	renderer.render(stage);
+}
+
+
+setup();
