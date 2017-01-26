b1fb41f0b87d37e7ffa4cd5dfa459cd6f4265002
Ammar Husain <ammo700@gmail.com>
2017-01-26 17:28:52 -0500

    Built barebones for stage builder, with grid and grid snapping

--- stagebuilder.js
+++ stagebuilder.js
@@ -18,14 +18,34 @@ var graphics = new Graphics();
 graphics.beginFill(0x000000);
 
 // set the line style to have a width of 5 and set the color to red
-graphics.lineStyle(1, 0xFF0000);
+graphics.lineStyle(0, 0xFF0000);
 
+var width = 720;
+var height = 720;
 // draw a rectangle
-graphics.drawRect(0, 0, 720, 720);
+graphics.drawRect(0, 0, width, height);
 
 stage.addChild(graphics);
 
-
+var gridInc = 10;
+var gridColor = 0x696969;
+function buildGrid(){
+    for(var i = 0; i <= width; i+= gridInc){
+        var graphic = new Graphics();
+        graphic.lineStyle(1, gridColor, 1);
+        graphic.moveTo(i,0);
+        graphic.lineTo(i, height);
+        stage.addChild(graphic);
+    }
+    for(var i = 0; i <= height; i+= gridInc){
+        var graphic = new Graphics();
+        graphic.lineStyle(1, gridColor, 1);
+        graphic.moveTo(0,i);
+        graphic.lineTo(width, i);
+        stage.addChild(graphic);
+    }
+    renderer.render(stage);
+}
 
 stage
     .on('mousedown', onButtonDown)
@@ -36,11 +56,30 @@ stage
     .on('touchendoutside', onButtonUp);
 
 renderer.render(stage);
+buildGrid();
+var init;
+var final;
+function onButtonDown(e){
+    console.log(e);
 
-function onButtonDown(){
-	console.log("onButtonDown");
+    init = [gridInc*Math.round(e.data.originalEvent.x/gridInc), gridInc*Math.round(e.data.originalEvent.y/gridInc)];
+    console.log(e.data.originalEvent.x + ", "+e.data.originalEvent.y);
 }
 
-function onButtonUp(){
-	console.log("onButtonUp");
+function onButtonUp(e){
+    console.log(e);
+	console.log(e.data.originalEvent.x + ", "+ e.data.originalEvent.y);
+    var graphic = new Graphics();
+ 
+    // begin a green fill..
+    graphic.lineStyle(1, 0xFFFFFF, 1);
+    // draw a triangle using lines
+    graphic.moveTo(init[0],init[1]);
+    graphic.lineTo(gridInc*Math.round(e.data.originalEvent.x/gridInc), gridInc*Math.round(e.data.originalEvent.y/gridInc));
+     
+    // end the fill
+     
+    // add it the stage so we see it on our screens..
+    stage.addChild(graphic);
+    renderer.render(stage);
 }
